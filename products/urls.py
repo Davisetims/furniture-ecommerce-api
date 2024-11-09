4de099960c84b5ad9a28d201454160c34a3213d9
DavisWere <devisodhis10@gmail.com>
2024-11-09 13:39:30 +0300

    "Added serve_3d_object view and URL pattern to serve 3D object files from MEDIA_ROOT with CORS headers"

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from products.views import CategoryViewSet, ProductViewSet
+from . import views
 
 product_router = DefaultRouter()
 product_router.register(r'categories', CategoryViewSet)
@@ -8,5 +9,7 @@ product_router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     
+    path(r'^media/(?P<path>.*\.obj)$', views.serve_3d_object),
+
 ]
 urlpatterns += product_router.urls
--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, permissions
+from django.http import HttpResponse
+from django.views.static import serve
+from django.conf import settings
 from products.models import Category,Product
 from products.serializers import CategorySerializer, ProductSerializer
 
@@ -9,4 +12,12 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+    
+def serve_3d_object(request, path):
+    response = serve(request, path, document_root=settings.MEDIA_ROOT)
+    response["Access-Control-Allow-Origin"] = "https://cessfuniture.netlify.app"
+    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response["Access-Control-Allow-Headers"] = "Content-Type"
+    return response
     
