81b705cab0fa6c3682e60d2137e277e9302e02f9
Davisetims <etimsintegration@gmail.com>
2024-12-11 10:01:55 +0300

    diplay id for customer

--- carts/admin.py
+++ carts/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 from carts.models import Cart, Payment
 
-class CartAdmin(admin.ModelAdmin):
-    list_display = ['cart_id', 'customer', 'items',  'created_at']
+# class CartAdmin(admin.ModelAdmin):
+#     list_display = ['cart_id', 'customer', 'items',  'created_at']
     
-    def cart_id(self,obj):
+#     def cart_id(self,obj):
+#         return obj.id
+#     cart_id.short_description = 'cart_id'
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['cart_id', 'customer_id', 'items', 'created_at']
+    
+    def cart_id(self, obj):
         return obj.id
-    cart_id.short_description = 'cart_id'
+    cart_id.short_description = 'Cart ID'
+
+    def customer_id(self, obj):
+        return obj.customer.id if obj.customer else 'No Customer'
+    customer_id.short_description = 'Customer ID'
+
+admin.site.register(Cart, CartAdmin)
     
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ['payment_id', 'customer', 'cart','payment_method', 'payment_date', 'payment_status', 'amount']
